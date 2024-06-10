e8bba91ef57395941336dc61df2f9a46eeaebc50
Jean Moraes <jean.moraes@t10.digital>
2024-06-10 14:46:43 +0100

    feat: base do componente NavBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import { Navbar } from './components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <p>TRATO TECH</p>
+    <Navbar />
   </React.StrictMode>
 );
