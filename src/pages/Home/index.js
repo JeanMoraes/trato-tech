7893099663daa0c9a6390a02225e72e5a60d6488
Jean Moraes <jean.moraes@t10.digital>
2024-06-10 17:46:26 +0100

    feat: listando as categorias na tela inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,12 @@ import Header from 'components/Header'
 import styles from './Home.module.scss'
 import relogio from 'assets/inicial.png'
 
+import { categories } from 'data/categories'
+import { useNavigate } from 'react-router-dom'
+
 export const Home = () => {
+    const navigate = useNavigate()
+
     return (
         <div>
             <Header
@@ -11,6 +16,23 @@ export const Home = () => {
                 img={relogio}
                 className={styles.header}
             />
+
+            <div className={styles.categorias}>
+                <div className={styles['categorias-title']}>
+                    <h1>Categorias</h1>
+                </div>
+
+                <div className={styles['categorias-container']}>
+                    {
+                        categories.map((category, index) => (
+                            <div key={index} onClick={() => navigate(`/categoria/${category.id}`)}>
+                                <img src={category.thumbnail} alt={category.name} />
+                                <h1>{category.nome}</h1>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
